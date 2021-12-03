e989d284ec361f24da2da13c1dc65f8438caade9
cobsccomp192p-001 <77620302+cobsccomp192p-001@users.noreply.github.com>
2021-12-03 20:06:23 +0530

    Basic navigation started using react router v5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hats</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route  path="/hats" component={HatsPage} />
+      </Switch>
+      {/* <HomePage/> */}
     </div>
   );
 }
