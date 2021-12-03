05446ff40d778e80c97592d247d56b5e012bcef7
cobsccomp192p-001 <77620302+cobsccomp192p-001@users.noreply.github.com>
2021-12-03 21:35:44 +0530

    Shops page with collection preview and items component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import React from "react";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats</h1>
-    </div>
-  );
-};
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route  path="/hats" component={HatsPage} />
+        <Route  path="/shop" component={ShopPage} />
       </Switch>
       {/* <HomePage/> */}
     </div>
