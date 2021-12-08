b458e69167f3749797e778e57df4fd21dabcf4d9
cobsccomp192p-001 <77620302+cobsccomp192p-001@users.noreply.github.com>
2021-12-08 12:51:36 +0530

    included ability to store authenticated user details into firestore, Sign up component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SigninAndSignupPage from "./pages/signin-and-signup/signin-and-signup.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth,createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -19,9 +19,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(this.state.currentUser);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if(userAuth){
+        const userRef=await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot=>{
+          this.setState({
+            currentUser:{
+              id:snapshot.id,
+              ...snapshot.data()
+            }
+          },()=>console.log(this.state));
+        });
+      }
+      else{
+        this.setState({currentUser:userAuth})
+      }
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,21 +1,45 @@
-import firebase from "firebase/compat/app"
-import "firebase/compat/auth"
-import "firebase/compat/firestore"
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-const config ={
-    apiKey: "AIzaSyBHXmvXbj2uzBpnhueNoYkOeSx8aCjTb4w",
+const config = {
+  apiKey: "AIzaSyBHXmvXbj2uzBpnhueNoYkOeSx8aCjTb4w",
   authDomain: "clothing-app-c7fd4.firebaseapp.com",
   projectId: "clothing-app-c7fd4",
   storageBucket: "clothing-app-c7fd4.appspot.com",
   messagingSenderId: "409190892532",
-  appId: "1:409190892532:web:442ee22cb5c2d7fc346fa1"
-}
-firebase.initializeApp(config);
-export const auth=firebase.auth();
-export const firestore=firebase.firestore();
+  appId: "1:409190892532:web:442ee22cb5c2d7fc346fa1",
+};
 
-const provider= new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt:'select_account'});
-export const signInWithGoogle=()=>auth.signInWithPopup(provider);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error creating user", error.message);
+    }
+  }
+  return userRef;
+};
+
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
