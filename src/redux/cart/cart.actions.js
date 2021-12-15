7ea0b0244589659181628336ff7223248679f76b
cobsccomp192p-001 <77620302+cobsccomp192p-001@users.noreply.github.com>
2021-12-15 21:15:41 +0530

    added the decerease and increase the quanitity functionality to checkout item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,4 +7,14 @@ import {cartActionTypes} from './cart.types';
  export const addItem=item=>({
     type:cartActionTypes.ADD_ITEM,
     payload:item
+ });
+
+ export const clearItem=item=>({
+    type:cartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload:item
+ })
+
+ export const removeItem=item=>({
+    type:cartActionTypes.REMOVE_ITEM,
+    payload:item
  })
